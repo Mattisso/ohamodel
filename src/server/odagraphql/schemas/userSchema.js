ffa5858e4596cbb0d96914ce22be821ee78c1c5f
Mensah A <attmensah@gmail.com>
2020-04-10 18:32:50 -0400

    init

--- src/server/odagraphql/schemas/userSchema.js
+++ src/server/odagraphql/schemas/userSchema.js
@@ -1,7 +1,9 @@
 const graphql = require('graphql');
-const {GraphQLObjectType,GraphQLString,GraphQLID,GraphQLInt} = graphql;
-const userSchemas=(function(){
+const {User} = require('../../omodels/modelsSchema/index').toinit();
+const {GraphQLObjectType,GraphQLString,GraphQLID
+  ,GraphQLSchema,GraphQLList, GraphQLNonNull,GraphQLInt} = graphql;
 
+const userSchema=(function(){
   const UserType = new GraphQLObjectType({
     name: 'User',
     fields: () => ({
@@ -22,16 +24,76 @@ const userSchemas=(function(){
         }
     })
   }); 
+
   
-  function toinit(){
-return {
-  UserType:UserType
-}
+const UserRootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+      getuser: {
+          type: UserType,
+          //argument passed by the user while making the query
+          args: {
+              id: {
+                  type: GraphQLID
+              }
+          },
+          resolve(parent, args) {
+              //Here we define how to get data from database source
+
+              //this will return the book with id passed in argument by the user
+              return User.findById(args.id);
+          }
+      },
+      getusers: {
+          type: new GraphQLList(UserType),
+          //argument passed by the user while making the query
+          resolve(parent, args) {
+              //Here we define how to get data from database source
+
+              //this will return the book with id passed in argument by the user
+              return User.find({});
+          }
+      }
+     
+  }
+});
+  const UserMutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                //GraphQLNonNull make these field required
+                username: { type: new GraphQLNonNull(GraphQLString) },
+                role: { type: new GraphQLNonNull(GraphQLString)},
+                password:{ type: new GraphQLNonNull(GraphQLString) }
+            },
+            resolve(parent, args) {
+                let user = new User({
+                    username: args.username,
+                    role: args.role,
+                    password: args.password
+                });
+                return user.save();
+            }
+        }
+       
+    }
+});
+
+
+  
+ function toinit(){
+const userresolver= new GraphQLSchema({
+  query: UserRootQuery,
+  mutation:UserMutation
+})
+return userresolver
   };
 return {
   toinit:toinit
 }
 })()
-module.exports={
-  toinit:userSchemas.toinit
+module.exports = {
+  toinit:userSchema.toinit
 }
